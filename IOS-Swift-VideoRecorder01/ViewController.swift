ba3e217da022553942c7a5851404cfce42d8e70d
Pooya Hatami <pooya@node-1w7jr9qro06swyxhj42o1s0zv.ipv6.telus.net>
2018-08-30 18:40:26 -0700

    done

--- IOS-Swift-VideoRecorder01/ViewController.swift
+++ IOS-Swift-VideoRecorder01/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  IOS-Swift-VideoRecorder01
 //
-//  Created by Pooya Hatami on 2018-03-12.
+//  Created by Pooya on 2018-03-12.
 //  Copyright © 2018 Pooya Hatami. All rights reserved.
 //
 
@@ -13,15 +13,12 @@ import MobileCoreServices
 class ViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
 
     @IBOutlet weak var RecordButton: UIButton!
-    
+    var videoAndImageReview = UIImagePickerController()
+    var videoURL: URL?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     @IBAction func RecordAction(_ sender: UIButton) {
@@ -33,7 +30,7 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
             imagePicker.mediaTypes = [kUTTypeMovie as String]
-            imagePicker.allowsEditing = false
+            imagePicker.allowsEditing = true
             
             self.present(imagePicker, animated: true, completion: nil)
         } else {
@@ -42,12 +39,47 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate , UINav
         
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        guard
+            let mediaType = info[UIImagePickerControllerMediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let url = info[UIImagePickerControllerMediaURL] as? URL,
+            UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+            else {
+                return
+        }
+        
+        // Handle a movie capture
+        UISaveVideoAtPathToSavedPhotosAlbum(
+            url.path,
+            self,
+            #selector(video(_:didFinishSavingWithError:contextInfo:)),
+            nil)
+    }
     
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+  
+    @IBAction func openImgVideoPicker() {
+        videoAndImageReview.sourceType = .savedPhotosAlbum
+        videoAndImageReview.delegate = self
+        videoAndImageReview.mediaTypes = ["public.movie"]
+        present(videoAndImageReview, animated: true, completion: nil)
+    }
     
-    
-    
-    
-    
-    
-}
+    func videoAndImageReview(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        videoURL = info[UIImagePickerControllerMediaURL] as? URL
+        print("videoURL:\(String(describing: videoURL))")
+        self.dismiss(animated: true, completion: nil)
+    }
 
+}
